a23264bf2c4ad9b388c058391aa3c2ef4c3cb213
hdelcarpiom <140546274+hdelcarpiom@users.noreply.github.com>
2024-03-30 13:09:06 -0500

    01

---
+++ Mouredev/01_Operadores_Estructuras_Control.py
@@ -0,0 +1,23 @@
+# pylint: disable=invalid-name
+#   EJERCICIO:
+#   - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+#     Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+#     (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+#   - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+#     que representen todos los tipos de estructuras de control que existan
+#     en tu lenguaje:
+#     Condicionales, iterativas, excepciones...
+#   - Debes hacer print por consola del resultado de todos los ejemplos.
+#  
+#   DIFICULTAD EXTRA (opcional):
+#   Crea un programa que imprima por consola todos los números comprendidos
+#   entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+#  
+#   Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+
+suma = 9 + 3
+operadorResta = 9 - 3
+multiplicacion = 9 * 3
+division = 9 / 3
+
+print("Suma : " 
